d8b98d2ded3d2c9f0b97adf8a7f6935a321470a5
ShurGith <75266693+ShurGith@users.noreply.github.com>
2025-07-14 21:04:37 +0200

    INICIO

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,19 +1,36 @@
 import { useState } from "react"
+import type { ChangeEvent, FormEvent } from "react"
 import { categories } from "../data/categories"
+import type { Activity } from "../types/"
 
 function Form() {
-    const [activity, setActivity] = useState({
+    const [activity, setActivity] = useState<Activity>({
         category: 1,
         name: '',
         calories: 0,
     })
-    const handleChange = (e)=>{
-        console.log(e.target.id);
-        console.log(e.target.value);
+    const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
+        const isNumberField = ['calories', 'category'].includes(e.target.id)
+        setActivity({
+            ...activity,
+            [e.target.id]: isNumberField ? +e.target.value : e.target.value
+        })
     }
+
+    const isValidActivity = () => {
+        return activity.name.trim().length > 0 && activity.calories > 0;
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log("Enviado....");
+        // TODO: Add form submission logic here
+    }
+
     return (
         <form
             className="space-y-5 bg-white shadow p-10 rounded-lg not-visited:"
+            onSubmit={handleSubmit}
         >
             <div className="grid grid-cols-1 gap-3">
                 <label htmlFor="category" className="font-bold">Categoría:</label>
@@ -22,7 +39,7 @@ function Form() {
                     id="category"
                     value={activity.category}
                     onChange={handleChange}
-                    >
+                >
                     {categories.map(category => (
                         <option
                             key={category.id}
@@ -42,10 +59,10 @@ function Form() {
                     value={activity.name}
                     onChange={handleChange}
                 />
-                </div>
+            </div>
             <div className="grid grid-cols-1 gap-3">
                 <label htmlFor="activity" className="font-bold">Calorias:</label>
-                 <input 
+                <input
                     type="number"
                     id="calories"
                     className="border border-slate-300 p-2 rounded-lg"
@@ -55,10 +72,11 @@ function Form() {
                 />
             </div>
             <input type="submit"
-            className="bg-gray-600 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
-            value="Guardar comida o ejercicio"
-           />
-            
+                className="bg-gray-600 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-20 disabled:cursor-not-allowed"
+                value={`Guardar ${activity.category === 1 && isValidActivity() ? 'Comida' : 'Ejercicio'}`}
+                disabled={!isValidActivity()}
+            />
+
         </form>
     )
 }
--- src/data/categories.ts
+++ src/data/categories.ts
@@ -2,7 +2,6 @@ import type {Category} from "../types"
 
 export const categories : Category[] = [
   { id: 1, name: 'Comida'},
-  { id: 2, name: 'Desayuno'},
-  { id: 3, name: 'Ejercicio'},
+  { id: 2, name: 'Ejercicio'},
 
 ]
