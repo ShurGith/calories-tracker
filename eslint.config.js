c5c08b620a413d828fbabe05f69aa10c92b9f3ff
ShurGith <75266693+ShurGith@users.noreply.github.com>
2025-07-14 17:19:26 +0200

    Inicio

--- eslint.config.js
+++ eslint.config.js
@@ -4,18 +4,29 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
-
+import reactX from 'eslint-plugin-react-x'
+import reactDom from 'eslint-plugin-react-dom'
 export default tseslint.config([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      //tseslint.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
       reactHooks.configs['recommended-latest'],
+      ...tseslint.configs.strictTypeChecked,
       reactRefresh.configs.vite,
+      ...tseslint.configs.stylisticTypeChecked,
+      reactX.configs['recommended-typescript'],
+      reactDom.configs.recommended,
+
     ],
     languageOptions: {
+       parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
       ecmaVersion: 2020,
       globals: globals.browser,
     },
---
+++ src/components/Form.tsx
@@ -0,0 +1,66 @@
+import { useState } from "react"
+import { categories } from "../data/categories"
+
+function Form() {
+    const [activity, setActivity] = useState({
+        category: 1,
+        name: '',
+        calories: 0,
+    })
+    const handleChange = (e)=>{
+        console.log(e.target.id);
+        console.log(e.target.value);
+    }
+    return (
+        <form
+            className="space-y-5 bg-white shadow p-10 rounded-lg not-visited:"
+        >
+            <div className="grid grid-cols-1 gap-3">
+                <label htmlFor="category" className="font-bold">Categoría:</label>
+                <select
+                    className="border border-slate-300 p-2 rounded-lg"
+                    id="category"
+                    value={activity.category}
+                    onChange={handleChange}
+                    >
+                    {categories.map(category => (
+                        <option
+                            key={category.id}
+                            value={category.id}>
+                            {category.name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+            <div className="grid grid-cols-1 gap-3">
+                <label htmlFor="name" className="font-bold">Actividad:</label>
+                <input
+                    type="text"
+                    id="name"
+                    className="border border-slate-300 p-2 rounded-lg"
+                    placeholder="Ej. Comida, Zumo de Naranja, Ensalada, Pesas, Bicicleta..."
+                    value={activity.name}
+                    onChange={handleChange}
+                />
+                </div>
+            <div className="grid grid-cols-1 gap-3">
+                <label htmlFor="activity" className="font-bold">Calorias:</label>
+                 <input 
+                    type="number"
+                    id="calories"
+                    className="border border-slate-300 p-2 rounded-lg"
+                    placeholder="Calorias, Ej....500, 300"
+                    value={activity.calories}
+                    onChange={handleChange}
+                />
+            </div>
+            <input type="submit"
+            className="bg-gray-600 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
+            value="Guardar comida o ejercicio"
+           />
+            
+        </form>
+    )
+}
+
+export default Form
